9464a3dd257f1f98d5ddd15988e1a1c03a86d6c7
Tarcizio Barbosa - Dislog <tarcizio.barbosa@outlook.com>
2021-10-04 17:30:18 -0300

    create a instance of prisma client

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,12 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
+export { }
